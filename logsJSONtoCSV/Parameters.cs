30d0a7c478bfc8f910cbfe6635d53575dd203038
Maill <nicolas.demoncourt@gmail.com>
2017-10-03 16:44:06 +0200

    Exercice rendu

---
+++ logsJSONtoCSV/Parameters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logsJSONtoCSV
+{
+    class Parameters
+    {
+        private static Parameters instance;
+
+        public String srcLogsPath;
+
+        public String outPath;
+
+        public static Parameters Instance()
+        {
+            if(instance == null)
+            {
+                instance = new Parameters();
+                return instance;
+            }
+            return instance;
+        }
+
+        public void ParseArguments(String[] args)
+        {
+            if (args.Count() == 0 || args.Count() == 1)
+            {
+                writeHelp();
+                return;
+            }
+
+            this.srcLogsPath = args[0];
+            this.outPath = args[1];
+
+        }
+
+        public void writeHelp()
+        {
+            Console.WriteLine("Using : jsonlogstocsv.exe json_path out_file_path");
+            Console.WriteLine(@"Exemple : jsonlgstosv.exe C:\data\json\logs.json C:\data\out\json\");
+        }
+    }
+}
---
+++ logsJSONtoCSV/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logsJSONtoCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Parameters param = Parameters.Instance();
+                param.ParseArguments(args);
+
+                if(param.srcLogsPath == null && param.outPath == null)
+                {
+                    Console.WriteLine("Invalid parameters.");
+                }
+
+                String contentFile = File.ReadAllText(param.srcLogsPath);
+
+                JObject JsonToObject = JObject.Parse(contentFile);
+                JArray arrayEntries = JArray.Parse(JsonToObject.Last.Last.ToString());
+
+                StringBuilder toWrite = new StringBuilder();
+
+                foreach (JObject line in arrayEntries)
+                {
+                    toWrite.Append(line.GetValue("code").ToString() + ";");
+                    toWrite.Append(line.GetValue("host").ToString() + ";");
+                    toWrite.Append(line.GetValue("path").ToString() + ";");
+                    toWrite.Append(line.GetValue("durationMs").ToString());
+                    toWrite.Append('\n');
+                }
+
+                string fileName = Path.GetFileNameWithoutExtension(param.srcLogsPath);
+                StreamWriter fileToWrite = File.CreateText(param.outPath + fileName + "-" + DateTime.Now.ToString("yyyyMMdd-HHMM") + ".csv");
+                fileToWrite.Write(toWrite.ToString());
+                fileToWrite.Flush();
+                fileToWrite.Dispose();
+            } catch (Exception exp)
+            {
+                Console.WriteLine("An error occured.\nMessage : " + exp.Message + "\nStrackTrace : " + exp.StackTrace);
+            }
+            
+        }
+    }
+}
